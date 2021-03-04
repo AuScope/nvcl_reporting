60049e0dace604167abbe5d4030118101ce06771
Vincent Fazio <vincent.fazio@csiro.au>
2021-03-04 12:22:05 +1100

    NVCL-60 Add To/From addresses for email script

--- email.sh
+++ email.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 [ ! -e .email_addr ] && echo "Missing '.email_addr' file with email address inside" && exit 1
-EM=`cat .email_addr`
-#python3 run_NVCL.py | mail -r $EM -s "NVCL Report" $EM
+# First address is 'To:' address, second is 'From:'
+TO_ADDR=`head -1 .email_addr`
+FROM_ADDR=`tail -1 .email_addr`
+mail -r $FROM_ADDR -s 'NVCL Report' -A report.pdf $TO_ADDR
