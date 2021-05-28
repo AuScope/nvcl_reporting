79ba2000ce47818d3508dc3190a4fc3e5121d47f
Vincent Fazio <vincent.fazio@csiro.au>
2021-05-28 12:02:11 +0800

    Include email in the reporting script

--- email.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#
-# Simple script used to send NVCL reports to people via email
-#
-[ ! -e .email_addr ] && echo "Missing '.email_addr' file with email address inside" && exit 1
-# First address is 'To:' address, second is 'From:'
-TO_ADDR=`head -1 .email_addr`
-FROM_ADDR=`tail -1 .email_addr`
-echo "   " | mail -r $FROM_ADDR -s 'NVCL Report' -A report.pdf $TO_ADDR
--- run_reports.sh
+++ run_reports.sh
@@ -15,4 +15,16 @@ echo "Weekly run"
 
 else
 echo "Usage: run_reports.sh [A|Q|W]"
+exit 1
 fi
+
+# Email
+if [ ! -e .email_addr ]; then
+echo "Missing 2 line '.email_addr' file with email addresses inside"
+echo "First line is 'To:' address, second is 'From:'"
+exit 1
+fi
+# First address is 'To:' address, second is 'From:'
+TO_ADDR=`head -1 .email_addr`
+FROM_ADDR=`tail -1 .email_addr`
+echo "   " | mail -r $FROM_ADDR -s 'NVCL Report' -A report.pdf $TO_ADDR
