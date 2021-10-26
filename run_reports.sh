d99e504cb3f5825fc00e22554b9868c9423492a0
Vincent Fazio <vincent.fazio@csiro.au>
2021-10-26 14:05:55 +0800

    Adapt for running with crontab

--- run_reports.sh
+++ run_reports.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
+#
+# Main script to run reports
+#
+# * Annual run: ./run_reports.sh A
+# * Quarterly run: ./run_reports.sh Q
+# * Weekly run: ./run_reports.sh W
+#
+# Go to reporting directory
+cd ~/gitlab/nvcl_reporting
 
-# Email
+# Check email
 if [ ! -e .email_addr ]; then
 echo "Missing 2 line '.email_addr' file with email addresses inside"
 echo "First line is 'To:' address, second is 'From:'"
 exit 1
 fi
+
+# Activate Python env
+. ./venv/bin/activate
 
 if [ "$1" = "A" ]; then
 # Takes a while, only run once a year (midnight June 30)
@@ -26,7 +38,7 @@ REPORT_NAME=report.pdf
 elif [ "$1" = "W" ]; then
 # Run every week, accumulates boreholes created in the past week
 echo "Weekly run"
-./run_NVCL.py -rsb
+./run_NVCL.py -rsb -d pkl-weekly
 REPORT_NAME=report-brief.pdf
 
 else
@@ -38,3 +50,4 @@ fi
 TO_ADDR=`head -1 .email_addr`
 FROM_ADDR=`tail -1 .email_addr`
 echo "   " | mail -r $FROM_ADDR -s 'NVCL Report' -A $REPORT_NAME $TO_ADDR
+exit 0
