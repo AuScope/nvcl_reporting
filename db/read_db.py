26bece6b8669d72f45b6c1c6f545816166f49de8
Vincent Fazio <vincent.fazio@csiro.au>
2023-03-28 15:55:40 +0800

    Create a script to read entire db

--- db/read_db.py
+++ db/read_db.py
@@ -4,25 +4,35 @@
 import sys
 import pandas as pd
 pd.options.mode.chained_assignment = None
+pd.set_option('display.max_columns', None)
 import sqlite3
 from collections import OrderedDict
 from types import SimpleNamespace
+from datetime import datetime
+from numpy import array
+import json
 
 
-def get_data(data_str, col_name):
-    if col_name == 'data':
-        return eval(data_str.replace('namespace', 'SimpleNamespace'))
-    return(eval(data_str))
+
+def conv_dt(dt_str):
+    return datetime.strptime(dt_str, '%Y/%m/%d')
+
+def conv_json(json_str):
+    return json.loads(json_str) 
 
 if __name__ == "__main__":
 
-    # Open up connection
-    #db.connect()
-    #records = Meas.get()
-    #print("resords=", records)
     con = sqlite3.connect("nvcl2.db")
     df = pd.read_sql("SELECT * from MEAS", con) 
-    for col in ['report_category', 'state', 'nvcl_id', 'create_datetime', 'log_id', 'algorithm', 'log_type', 'algorithmID', 'minerals', 'mincnts', 'data']:
-        print(repr(get_data(df[col][0], col)), type(get_data(df[col][0], col)))
-    #for v in get_data(df['data'][6]).values():
-    #    print(v.classText)
+    new_df = pd.DataFrame()
+    for col in df.columns:
+        print(f"converting {col}")
+        if col == 'create_datetime':
+            new_df[col] = df[col].apply(conv_dt)
+        elif col in ['minerals', 'mincnts', 'data']:
+            new_df[col] = df[col].apply(conv_json)
+        else:
+            new_df[col] = df[col]
+
+    #['report_category', 'state', 'nvcl_id', 'create_datetime', 'log_id', 'algorithm', 'log_type', 'algorithmID', 'minerals', 'mincnts']:
+    print(new_df)
