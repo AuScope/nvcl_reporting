fa4b8da61560adb6b3270681ece1d806c3bb5ce9
Vincent Fazio <vincent.fazio@csiro.au>
2024-09-23 07:42:14 +0800

    Exit script if no report file found

--- scripts/run_reports.sh
+++ scripts/run_reports.sh
@@ -58,9 +58,15 @@ echo "Usage: run_reports.sh [A|Q|W]"
 exit 1
 fi
 
+if [ -e $REPORT_NAME ]; then
 echo "Sending report email"
-# First line is 'To:' addresses
+# First line contains 'To:' addresses
 TO_ADDR=`head -1 .email_addr`
 # Send reports via email using mutt
 echo "Please find attached $FREQUENCY NVCL Report" | mutt -s "$FREQUENCY NVCL Report" -a $REPORT_NAME -- $TO_ADDR
 exit 0
+
+else
+echo "Report file $REPORT_NAME was not generated. Exiting."
+exit 1
+fi
