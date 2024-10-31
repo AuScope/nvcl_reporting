423439f20bd5325b341dfcde6d8079876246f220
Vincent Fazio <vincent.fazio@csiro.au>
2024-10-31 14:32:39 +0800

    Fix CLI tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -16,7 +16,6 @@ from make_reports import main
          (["-b", "-c", "missing.yaml"], "Cannot find config file missing.yaml"),
          (["-b", "-c", "data/config.yaml", "-r", "1234567890"], "Report date has incorrect format:"),
          (["-b", "-c", "data/config.yaml", "-d", "missing.db"], "Cannot find data in database missing.db"),
-         (["-b", "-c", "data/config.yaml"], "unable to open database file"),
          (["-b", "-c", "data/empty_config.yaml"], "data/empty_config.yaml, it is empty"),
          (["-b", "-c", "data/missing_config.yaml"], "data/missing_config.yaml is missing a value for 'tsg_meta_file'"),
          (["-b", "-c", "data/corrupted_config.yaml"], "Error in configuration file:"),
