6b93a2f0eb01fe0bf2e981f039d8bb02770e82a4
Vincent Fazio <vjf@users.noreply.github.com>
2025-10-07 13:02:38 +0800

    Complete Grafana backup script

--- grafana/backup.sh
+++ grafana/backup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 #
 # Script to perform logical backup of Grafana dashboards and datasources
+# This uses grizzly tool to export the config as JSON files
 # From:  https://grafana.github.io/grizzly/workflows/
 #
 grr config create-context source
 grr config set grafana.url http://localhost:3000
 grr config set grafana.token <insert API token here>
 grr config set targets Dashboard,Dashboardfolder,Datasource
+grr pull resources
