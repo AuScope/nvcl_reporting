99519aa6a6099b185fdaa7f7f692347014e9126a
Vincent Fazio <vjf@users.noreply.github.com>
2026-07-03 15:48:10 +1000

    Fix CLI tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -7,6 +7,8 @@ import pytest
 src_path = os.path.join(os.path.abspath(os.pardir), 'src')
 sys.path.insert(0, src_path)
 
+my_dir = os.path.dirname(os.path.abspath(__file__))
+
 from make_reports import main
 
 @pytest.mark.parametrize("cli_params, expected_out",
@@ -14,13 +16,15 @@ from make_reports import main
          (["-c", "dummy"], "No procedural command line options were selected"),
          (["--full", "-brief"], "Cannot select both full and brief report. Please select one or the other."),
          (["-b", "-c", "missing.yaml"], "Cannot find config file missing.yaml"),
-         (["-b", "-c", "data/config.yaml", "-r", "1234567890"], "Report date has incorrect format:"),
-         (["-b", "-c", "data/config.yaml", "-d", "missing.db"], "Cannot find data in database missing.db"),
-         (["-b", "-c", "data/empty_config.yaml"], "data/empty_config.yaml, it is empty"),
-         (["-b", "-c", "data/missing_config.yaml"], "data/missing_config.yaml is missing a value for 'tsg_meta_file'"),
-         (["-b", "-c", "data/corrupted_config.yaml"], "Error in configuration file:"),
+         (["-b", "-c", os.path.join(my_dir, "data/config.yaml"), "-r", "1234567890"], "Report date has incorrect format:"),
+         (["-b", "-c", os.path.join(my_dir, "data/empty_config.yaml")], "data/empty_config.yaml, it is empty"),
+         (["-b", "-c", os.path.join(my_dir, "data/missing_config.yaml")], "data/missing_config.yaml is missing a value for 'tsg_meta_file'"),
+         (["-b", "-c", os.path.join(my_dir, "data/corrupted_config.yaml")], "Error in configuration file:"),
         ])
 def test_eval(capsys, cli_params, expected_out):
+    os.environ["POSTGRES_DB"] = "dummy"
+    os.environ["POSTGRES_USER"] = "dummy"
+    os.environ["POSTGRES_PASSWORD"] = "dummy"
     with pytest.raises(SystemExit):
         main(["make_reports.py"] + cli_params)
     captured = capsys.readouterr()
