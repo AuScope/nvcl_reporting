0724f35a9fa9781811782e9c326337d373cd3581
Vincent Fazio <vjf@users.noreply.github.com>
2026-08-21 14:32:13 +1000

    Improve K8s manual db update script

--- k8s/tools/run_manual_update.sh
+++ k8s/tools/run_manual_update.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 # Script to start a manual database update
+# 
+# Edit "db-update-cronjob.sh" replacing variable references with working values
 # Run this script from the "tools" directory
+# 
 cd ../nvcl-report-app/templates
 kubectl delete jobs manual-run-001
 kubectl delete configmap my-script
-kubectl create configmap my-script --from-file=script.sh
+kubectl create configmap my-script --from-file=db_update.sh
 kubectl apply -f db-update-cronjob.yaml
 kubectl create job --from=cronjob.batch/run-db-update manual-run-001
